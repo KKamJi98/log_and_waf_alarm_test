f8d143b0a2b56a7f1b1156f834b1ea56db7fb41d
KKamJi <xowl5460@naver.com>
2024-12-05 00:02:24 +0900

    :memo:docs: black code formatting

--- frontend_traffic_generator.py
+++ frontend_traffic_generator.py
@@ -8,13 +8,14 @@ import aiohttp
 import asyncio
 from argparse import ArgumentParser
 
+
 async def send_request(session, url):
     """비동기적으로 요청 전송"""
     try:
         async with session.get(url) as response:
             # 상태코드 확인
             # print(f"Request to {url} - Status: {response.status}")
-            
+
             # CloudFront의 X-Cache 헤더 확인
             x_cache = response.headers.get("X-Cache", "Unknown")
             if x_cache == "Error from cloudfront":
@@ -24,7 +25,10 @@ async def send_request(session, url):
     except aiohttp.ClientError as e:
         print(f"Error sending request to {url}: {e}")
 
-async def generate_traffic(url, requests_per_interval, interval_seconds, duration_seconds):
+
+async def generate_traffic(
+    url, requests_per_interval, interval_seconds, duration_seconds
+):
     """지정된 URL로 요청을 초 단위로 전송"""
     total_intervals = duration_seconds // interval_seconds
     async with aiohttp.ClientSession() as session:
@@ -37,8 +41,11 @@ async def generate_traffic(url, requests_per_interval, interval_seconds, duratio
 
             # 초 단위 대기
             if interval < total_intervals - 1:  # 마지막 interval 이후에는 대기하지 않음
-                print(f"Waiting for {interval_seconds} seconds before the next interval.")
+                print(
+                    f"Waiting for {interval_seconds} seconds before the next interval."
+                )
                 await asyncio.sleep(interval_seconds)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Generate traffic to a frontend.")
@@ -62,6 +69,9 @@ if __name__ == "__main__":
 
     asyncio.run(
         generate_traffic(
-            args.url, args.requests_per_interval, args.interval_seconds, args.duration_seconds
+            args.url,
+            args.requests_per_interval,
+            args.interval_seconds,
+            args.duration_seconds,
         )
     )
